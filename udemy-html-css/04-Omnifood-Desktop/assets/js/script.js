b2b9f87a02f17b650e4264b6ae5ee4e92e42e42d
Artem Usatyi <youfunose@gmail.com>
2022-11-22 21:59:06 +0200

    Making the mobile navigation work!

--- udemy-html-css/04-Omnifood-Desktop/assets/js/script.js
+++ udemy-html-css/04-Omnifood-Desktop/assets/js/script.js
@@ -14,9 +14,21 @@ h1.style.padding = '5rem';
 });
 */
 
+/// ////////////////////////////////////////////////////////
+// Set current year in footer copyright section
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+/// ////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNav = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNav.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
 
 /// ////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
